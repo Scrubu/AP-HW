a019efa098483646c11f27fb648c0e75b23bd32b
Scrubu <kylelai2016@gmail.com>
2014-12-02 19:14:31 -0500

    driver for wordsearch

---
+++ 03wordsearch/Driver.java
@@ -0,0 +1,22 @@
+import java.io.FileNotFoundException;
+public class Driver{
+    public static void main(String[]args){
+	int row = Integer.parseInt(args[0]);
+	int column = Integer.parseInt(args[1]);
+	int seed = Integer.parseInt(args[2]);
+	int answer = Integer.parseInt(args[3]);
+	WordSearch a=new WordSearch(row,column,seed,answer);
+	boolean show=false;
+	if(answer==0){
+	    show = true;
+	}
+	a.loadWordsFromFile("words.txt",show);
+	a.toString();
+
+    }
+}
+//	    if(dx==-1){
+//		if(word.length()>horizontal-column+1
+//	    }
+//	    }true;
+
